33e626a083df86b5cdc9e0108cd5bc2d2a96397c
Javier Aceña <40375118+j0nl1@users.noreply.github.com>
2020-04-12 19:00:31 +0200

    Feature/mongodb (#3)
    * Created MongoDB loader
    
    * Init mongodb loader

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,10 @@
 // Environment
 export const PRODUCTION = 'PRODUCTION';
 
+// Protocols
+export const MONGO_SECURE = 'mongodb+srv:';
+export const MONGO_INSECURE = 'mongodb:';
+
 // Routes
 export const PREFIX_API_URL = '/api';
 
