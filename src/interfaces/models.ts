e25f1f123e47357454ae6d830926348b644650d5
j0nl1 <jonlitech@gmail.com>
2020-04-12 21:07:38 +0200

    Created user interface

---
+++ src/interfaces/models.ts
@@ -0,0 +1,11 @@
+import { Document } from 'mongoose';
+
+export interface IUserModel extends Document {
+  email: string;
+  password: string;
+  role: string;
+  username: string;
+  fullName: string;
+  bio: string;
+  image: string;
+}
